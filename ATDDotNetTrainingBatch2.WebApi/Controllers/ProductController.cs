4a18e30b038a10824dc9cfc2633533ceed7457ad
AyeThida-kawlin <ayethida540@gmail.com.com>
2025-07-26 13:42:07 +0630

    feat: API

---
+++ ATDDotNetTrainingBatch2.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATDDotNetTrainingBatch2.WebApi.Controllers
+{
+    // api/product
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //process
+            //data
+            return Ok("GET");
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            //process
+            //data
+            return Ok("CREATE");
+        }
+
+        [HttpPut]
+        public IActionResult Upset()
+        {
+            //process
+            //data
+            return Ok("UPSET");
+        }
+
+        [HttpPatch]
+        public IActionResult Update()
+        {
+            //process
+            //data
+            return Ok("UPDATE");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            //process
+            //data
+            return Ok("DELETE");
+        }
+    }
+}
