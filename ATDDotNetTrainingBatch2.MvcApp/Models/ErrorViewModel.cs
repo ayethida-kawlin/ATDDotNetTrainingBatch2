773b3e074efaefa257732541f5daeba51d0bf51f
AyeThida-kawlin <ayethida540@gmail.com.com>
2025-09-07 20:09:11 +0630

    feat: localstorage

---
+++ ATDDotNetTrainingBatch2.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATDDotNetTrainingBatch2.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
