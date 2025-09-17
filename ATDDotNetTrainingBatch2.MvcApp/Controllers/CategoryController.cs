256e60051b21074625b6424099758a93fb7b1818
AyeThida-kawlin <ayethida540@gmail.com.com>
2025-09-18 00:22:17 +0630

    feat: Northwind

--- ATDDotNetTrainingBatch2.MvcApp/Controllers/CategoryController.cs
+++ ATDDotNetTrainingBatch2.MvcApp/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using ATDDotNetTrainingBatch2.MVC_Blog.Database.AppDbContextModels;
+using ATDDotNetTrainingBatch2.MvcApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -44,6 +45,52 @@ namespace ATDDotNetTrainingBatch2.MvcApp.Controllers
             //}
             return Json(lst);
         }
+
+        [ActionName("Create")]
+        public IActionResult CategoryCreate() 
+        {
+            return View("CategoryCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> CategorySave(CategoryViewModel requestModel)
+        {
+            await _db.TblCategories.AddAsync(new TblCategory
+            {
+                CategoryId = Ulid.NewUlid().ToString(),
+                CategoryCode = requestModel.CategoryCode,
+                CategoryName = requestModel.CategoryName,
+            });
+
+            var result = await _db.SaveChangesAsync();
+            var model = new CategoryViewResponseModel
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Saving Successful" : "Saving Failed"
+            };
+            return Json(model);
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public async Task<IActionResult> CategoryEdit(string id)
+        {
+            var item = await _db.TblCategories.FirstOrDefaultAsync(x => x.CategoryId == id);
+            if (item is null)
+            {
+                return RedirectToAction("Index");
+            }
+            var model = new CategoryEditRequestModel
+            {
+                CategoryId = item.CategoryId,
+                CategoryCode = item.CategoryCode,
+                CategoryName = item.CategoryName
+            };
+
+            return Json(model);
+        }
+
     }
 
     public class CategoryViewModel
@@ -51,5 +98,19 @@ namespace ATDDotNetTrainingBatch2.MvcApp.Controllers
         public string CategoryId { get; set; }
         public string CategoryCode { get; set; } = string.Empty;
         public string CategoryName { get; set; } = string.Empty;
+    }
+
+    public class CategoryViewResponseModel
+    {
+        public bool IsSuccess { get; set; } 
+        public string Message { get; set; }
+        
+    }
+
+    public class CategoryEditRequestModel
+    {
+        public string CategoryId { get; set; }
+        public string CategoryCode { get; set; }
+        public string CategoryName { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ dotnet ef dbcontext scaffold "Server=.;Database=DotNetTrainingBatch2MiniPOS;User
 dotnet ef dbcontext scaffold "Server=.;Database=DotNetTrainingBatch2MiniPOS;User ID=sa;Password=sasa@123;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o AppDbContextModels -c AppDbContext -f
 
 dotnet ef dbcontext scaffold "Server=.;Database=TechnortalBatch2;User ID=sa;Password=sasa@123;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o AppDbContextModels -c AppDbContext -f
+
+
+dotnet ef dbcontext scaffold "Server=.;Database=Northwind;User ID=sa;Password=sasa@123;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o AppDbContextModels -c AppDbContext -f
